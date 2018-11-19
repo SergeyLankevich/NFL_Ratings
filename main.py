647d2bfd1c75c5c44de2d497cb937b8895b2902b
SergeyLankevich <sergeylankevich@gmail.com>
2018-11-20 03:40:16 +0700

    FINALE!

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import urllib.request
-url = 'http://www.nfl.com/player/derekanderson/2506546/profile'
-f = urllib.request.urlopen(url)
-s = f.read()
-text = str(s)
+a = open('input.txt', 'r')
 
-part_name = text.find("player-name")
-name = text[text.find('>', part_name)+1:text.find('&', part_name)]
-print(name)
+with open('output.txt','w') as f_out:
+    for line in a:
+        f = urllib.request.urlopen(line)
+        s = f.read()
+        text = str(s)
 
-part_rate = text.find('player-totals')
-statistics = text[text.find('<td>', part_rate) + 4:text.find('</tr>',part_rate)]
-pure_stats = str(statistics).replace('/', ' ')
-for the_char in pure_stats:
-    if the_char.isdigit() is False and the_char != ' ' and the_char != '.':
-        pure_stats = pure_stats.replace(the_char, '')
-print(pure_stats) # Если профиль игрока полноценный, то должно выводить 16 параметров;
-#  из этой длинной строки нам нужно вытащить 1-ый, 2-ой, 4-ый, 6-ой, 7-ой и 10-ый - это сам рейтинг.
+        part_name = text.find('player-name')
+        name = text[text.find('>', part_name) + 1: text.find('&', part_name)]
 
+        part_rate = text.find('player-totals')
+        total = text[text.find('TOTAL</td>'):text.find('</table>', text.find('TOTAL</td>'))]
+
+        table_results = []
+        for i in range(10):
+            table_results.append((total[total.find('<td>') + 4:total.find('</td>')]).replace(',',''))
+            total = total[total.find('</td>') + 4:]
+            i += 1
+        COMP = table_results[0]
+        ATT = table_results[1]
+        YDS = table_results[3]
+        TD = table_results[5]
+        _INT = table_results[6]
+        ratings = table_results[9]
+
+        print('{:<20}{:<5}{:<10}{:<10}{:<10}{:<10}{:<5}'.format(name, COMP, ATT, YDS, TD, _INT, ratings), file=f_out)
 
 
 
